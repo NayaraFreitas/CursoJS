05aed67940be14d38a8ecc6842718c02dd05fbbf
Nayara Freitas <nayarafreitasteles14@gmail.com>
2023-08-02 19:17:17 -0300

    More file in folder 8

---
+++ 8-Tooling e ES6 Modules/Core.js regenerator-runtime/webpack.config.js
@@ -0,0 +1,28 @@
+//
+const path = require('path') //CommonJs - padrão de modulo do Node.js
+
+module.exports = {
+    mode: 'development',
+    entry: './src/main.js',
+    output: {
+      path: path.resolve(__dirname, 'public', 'assets', 'js'), 
+      filename: 'bundle.js'
+    } ,
+
+    module: {
+        rules: [{
+            exclude: /node_modules/,  //pra ele não analisar o node-modules
+            test: /\.js$/, // ele irá analisar arquivos com .js
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/env']
+                }
+            }
+        }, {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }]
+    },
+    devtool: 'source-map'
+};
